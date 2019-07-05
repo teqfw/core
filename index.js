71ca9aba64e97669b094e86c5eda03a57f633d16
Alex Gusev <alex@flancer64.com>
2019-07-05 10:50:38 +0300

    WF-108 Define app initialization section

---
+++ index.js
@@ -0,0 +1,66 @@
+"use strict";
+const NS = "teqfw_core_all";
+
+
+/** =============================================================================
+ * Import.
+ * =========================================================================== */
+const commander = require("commander");
+const mod_scanner = require("./src/modScanner");
+
+
+/** =============================================================================
+ * Definitions of working elements (constants, variables, functions).
+ * =========================================================================== */
+const teqfw = global["teqfw"];
+const version = teqfw.version;
+
+/**
+ *
+ * @returns {Readonly<{run: run}>}
+ * @constructor
+ */
+function Construct() {
+    let _commander = commander;
+    _commander.version(version, "-v, --version");
+
+    function run() {
+        init(() => {
+            _commander.parse(process.argv);
+            if (!process.argv.slice(2).length) {
+                _commander.outputHelp();
+            }
+        });
+    };
+
+    function init(callback) {
+        const fnCliFoo = function () {
+            console.log("Foo is here...");
+        };
+        // scan all node_modules and compose list of TeqFW modules
+        mod_scanner((modules_list) => {
+            console.log("TeqFW modules: " + JSON.stringify(modules_list));
+            _commander.option("--foo", "command `Foo`", fnCliFoo);
+            callback();
+        });
+
+    }
+
+    return Object.freeze({
+        run
+    });
+}
+
+
+/** =============================================================================
+ * Objects composition.
+ * =========================================================================== */
+const teqfw_core_all_app = new Construct();
+
+
+/**
+ * Module exports.
+ * @public
+ */
+module.exports = teqfw_core_all_app;
+
---
+++ src/modScanner.js
@@ -0,0 +1,59 @@
+/**
+ * Scan modules in `node_modules` and compose list of TeqFW modules.
+ */
+"use strict";
+const NS = "teqfw_core_all_modScanner";
+
+
+/** =============================================================================
+ * Import.
+ * =========================================================================== */
+const fs = require("fs");
+
+/** =============================================================================
+ * Definitions of working elements (constants, variables, functions).
+ * =========================================================================== */
+const teqfw = global["teqfw"];
+
+function scan(callback) {
+    const result = [];
+    const dir_node_modules = teqfw.path.root + "/node_modules";
+    // read all folders in `./node_modules/``
+    fs.readdir(dir_node_modules, (err, dirs) => {
+        if (err) throw err;
+        let itemsProcessed = 0;
+        dirs.forEach((item, index, array) => {
+            // read `package.json` for every module
+            const file_package = dir_node_modules + "/" + item + "/package.json";
+            fs.stat(file_package, (err, stats) => {
+                if (err) {
+                    // skip folders w/o package.json
+                    itemsProcessed += 1;
+                } else {
+                    if (stats.isFile()) {
+                        // parse `package.json` and find `teqfw` node inside
+                        fs.readFile(file_package, (err, rawdata) => {
+                            itemsProcessed += 1;
+                            if (err) throw err;
+                            let package_json = JSON.parse(rawdata);
+                            if (package_json.teqfw) {
+                                // `teqfw` node means that module is TeqFW module
+                                result.push(file_package);
+                            }
+                            if (itemsProcessed >= array.length) {
+                                callback(result);
+                            }
+                        });
+                    } else {
+                        itemsProcessed += 1;
+                    }
+                }
+            });
+        });
+    });
+}
+
+/** =============================================================================
+ * Export
+ * =========================================================================== */
+module.exports = scan;
