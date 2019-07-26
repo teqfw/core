fb5a2c47d456601567ec06e8bfd5f2a5ca23e658
Alex Gusev <alex@flancer64.com>
2019-07-26 18:14:53 +0300

    WF-149 Mobile UI prototype

--- pub/service-worker.js
+++ pub/service-worker.js
@@ -1,22 +1,21 @@
 "use strict";
 
-var CACHE = 'teqfw-cache';
+const CACHE = "teqfw-cache";
 
 self.addEventListener("install", (evt) => {
     console.log("[ServiceWorker] Install");
+    log_storage_state();
     self.skipWaiting();
 });
 
 self.addEventListener("activate", (evt) => {
     console.log("[ServiceWorker] Activate");
+    log_storage_state();
     self.clients.claim();
 });
 
 self.addEventListener("fetch", (evt) => {
     console.log("[ServiceWorker] Fetch", evt.request.url);
-    log_storage_state();
-
-    evt.respondWith(fetch_resource(evt));
 });
 
 
@@ -28,24 +27,4 @@ function log_storage_state() {
         const json = JSON.stringify(estimate);
         console.log("storage:" + json);
     });
-}
-
-async function fetch_resource(event) {
-    // Try to get the response from a cache.
-    const cache = await caches.open(CACHE);
-    const cachedResponse = await cache.match(event.request);
-
-    if (cachedResponse) {
-        console.log(`"${event.request.url}" is got from cache.`);
-        return cachedResponse.clone();
-    }
-
-    const result = await fetch(event.request);
-    if (result && result.ok) {
-        // If result was ok save it to cache
-        // https://stackoverflow.com/a/37747473/4073821
-        await cache.put(event.request, result.clone());
-        console.log(`"${event.request.url}" is put to the cache.`);
-    }
-    return result;
 }
