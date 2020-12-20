f2066c907078df915cc201e0391f1ffbb76c58e1
Alex Gusev <alex@flancer64.com>
2020-12-20 18:35:26 +0200

    CAM-18 Add db-upgrade script to install user reg structures

---
+++ src/Util/Store/RDb.mjs
@@ -0,0 +1,51 @@
+/**
+ * Create name for foreign key constraint.
+ *
+ * @param {String} tblSrc
+ * @param {String|String[]} fldSrc
+ * @param {String} tblTrg
+ * @param {String|String[]} fldTrg
+ * @return {string}
+ * @exports TeqFw_Core_App_Util_Store_RDb_NameForForeignKey
+ */
+function TeqFw_Core_App_Util_Store_RDb_NameForForeignKey(tblSrc, fldSrc, tblTrg, fldTrg) {
+    let result = `FK_${tblSrc}_`;
+    // type of fldSrc should correspond to type of fldTrg
+    if (typeof fldSrc === 'string') {
+        result += `_${fldSrc}__${tblTrg}__${fldTrg}`;  // tblSrc__col__tblTrg__col
+    } else if (Array.isArray(fldSrc)) { // tblSrc__col1_col2__tblTrg__col1_col2
+        for (const one of fldSrc) {
+            result += `_${one}`
+        }
+        result += `__${tblTrg}_`;
+        for (const one of fldTrg) {
+            result += `_${one}`
+        }
+    }
+    return result;
+}
+
+/**
+ * Create name for unique key constraint.
+ *
+ * @param {String} tbl
+ * @param {String|String[]} fld
+ * @return {string}
+ * @exports TeqFw_Core_App_Util_Store_RDb_NameForUniqueKey
+ */
+function TeqFw_Core_App_Util_Store_RDb_NameForUniqueKey(tbl, fld) {
+    let result = `UQ_${tbl}_`;
+    if (typeof fld === 'string') {
+        result += `_${fld.toLowerCase()}`;
+    } else if (Array.isArray(fld)) {
+        for (const one of fld) {
+            result += `_${one.toLowerCase()}`
+        }
+    }
+    return result;
+}
+
+export {
+    TeqFw_Core_App_Util_Store_RDb_NameForForeignKey as NameForForeignKey,
+    TeqFw_Core_App_Util_Store_RDb_NameForUniqueKey as NameForUniqueKey,
+};
