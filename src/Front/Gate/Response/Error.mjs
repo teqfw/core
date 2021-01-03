17ffebd975be2787f383bf17654a478650898f69
Alex Gusev <alex@flancer64.com>
2021-01-03 21:46:55 +0200

    LEAN-104 Use composition to extend user session in ACL module

--- src/Front/Gate/Response/Error.mjs
+++ src/Front/Gate/Response/Error.mjs
@@ -1,4 +1,4 @@
-export default class TeqFw_Core_Front_Gate_Response_Error {
+export default class TeqFw_Core_App_Front_Gate_Response_Error {
     /** @type {*} original error data  */
     error
     /** @type {String} */
--- src/Obj/Factory.mjs
+++ src/Obj/Factory.mjs
@@ -1,6 +1,24 @@
 export default class TeqFw_Core_App_Obj_Factory {
 
     /**
+     * Copy own methods from 'source' to 'target'.
+     *
+     * @param {Object} target
+     * @param {Object} source
+     */
+    assignObjectMethods(target, source) {
+        const methods = Object.getOwnPropertyNames(source)
+            .filter(
+                p => (typeof source[p] === 'function' && p !== 'constructor')
+            );
+        const methodsToAssign = {};
+        for (const one of methods) {
+            methodsToAssign[one] = source[one];
+        }
+        Object.assign(target, methodsToAssign);
+    }
+
+    /**
      * Copy methods from source.__proto__ to target __proto_ (excluding constructor).
      *
      * @param {Object} target
