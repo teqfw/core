9def1fa7d619950c6cfda9da18f97c8a876bb3da
Alex Gusev <alex@flancer64.com>
2021-03-26 13:05:32 +0200

    MF-54 Share referral link from friends route

--- src/Front/Data/Config.mjs
+++ src/Front/Data/Config.mjs
@@ -1,6 +1,6 @@
 export default class TeqFw_Core_App_Front_Data_Config {
     /**
-     * Base URL for frontend realms ('leana.home.teqfw.com').
+     * Base URL for frontend realms ('site.domain.com').
      * @type {String}
      */
     urlBase;
