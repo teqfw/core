4a94af08bd0aad16ad4eb7927880f20e6072dabd
Alex Gusev <alex@flancer64.com>
2021-04-01 18:23:22 +0300

    MF-58 Move Sign Up form to sign realm

--- src/Shared/Util.mjs
+++ src/Shared/Util.mjs
@@ -1,4 +1,36 @@
 /**
+ * Deep merge of the 2 objects.
+ * Source: https://gist.github.com/ahtcx/0cd94e62691f539160b32ecda18af3d6#gistcomment-2930530
+ *
+ * @param {Object} target
+ * @param {Object}source
+ * @returns {Object}
+ * @memberOf TeqFw_Core_App_Shared_Util
+ */
+function deepMerge(target, source) {
+    const isObject = (obj) => obj && typeof obj === 'object';
+
+    if (!isObject(target) || !isObject(source)) {
+        return source;
+    }
+
+    Object.keys(source).forEach(key => {
+        const targetValue = target[key];
+        const sourceValue = source[key];
+
+        if (Array.isArray(targetValue) && Array.isArray(sourceValue)) {
+            target[key] = targetValue.concat(sourceValue);
+        } else if (isObject(targetValue) && isObject(sourceValue)) {
+            target[key] = deepMerge(Object.assign({}, targetValue), sourceValue);
+        } else {
+            target[key] = sourceValue;
+        }
+    });
+
+    return target;
+}
+
+/**
  * @namespace TeqFw_Core_App_Shared_Util
  */
 
@@ -40,6 +72,24 @@ function formatDateTime(dateIn = null) {
 }
 
 /**
+ * Convert UTC time to HH:MM:SS.
+ * @param {Date|string|null} dateIn
+ * @return {string}
+ * @memberOf TeqFw_Core_App_Shared_Util
+ */
+function formatUtcTime(dateIn = null) {
+    /** @type {Date} */
+    const date = (dateIn) ?
+        (dateIn instanceof Date) ? dateIn : new Date(dateIn) :
+        new Date();
+    const h = `${date.getUTCHours()}`.padStart(2, '0');
+    const i = `${date.getUTCMinutes()}`.padStart(2, '0');
+    const s = `${date.getUTCSeconds()}`.padStart(2, '0');
+    return `${h}:${i}:${s}`;
+}
+
+
+/**
  * Return 'true' if `val` is empty.
  * @param {*} val
  * @returns {boolean}
@@ -49,42 +99,10 @@ function isEmpty(val) {
     return (val === undefined) || (val === null) || (val === '');
 }
 
-
-/**
- * Deep merge of the 2 objects.
- * Source: https://gist.github.com/ahtcx/0cd94e62691f539160b32ecda18af3d6#gistcomment-2930530
- *
- * @param {Object} target
- * @param {Object}source
- * @returns {Object}
- * @memberOf TeqFw_Core_App_Shared_Util
- */
-function deepMerge(target, source) {
-    const isObject = (obj) => obj && typeof obj === 'object';
-
-    if (!isObject(target) || !isObject(source)) {
-        return source;
-    }
-
-    Object.keys(source).forEach(key => {
-        const targetValue = target[key];
-        const sourceValue = source[key];
-
-        if (Array.isArray(targetValue) && Array.isArray(sourceValue)) {
-            target[key] = targetValue.concat(sourceValue);
-        } else if (isObject(targetValue) && isObject(sourceValue)) {
-            target[key] = deepMerge(Object.assign({}, targetValue), sourceValue);
-        } else {
-            target[key] = sourceValue;
-        }
-    });
-
-    return target;
-}
-
 export {
     deepMerge,
     formatDate,
     formatDateTime,
+    formatUtcTime,
     isEmpty,
 };
