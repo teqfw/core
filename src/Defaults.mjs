c43d41fcaa51c1bcafa28d89d103ae37a3ae43e6
Alex Gusev <alex@flancer64.com>
2021-04-20 17:14:49 +0300

    CAM-33 Place sign-in form to the route and generate sign-in link

--- src/Defaults.mjs
+++ src/Defaults.mjs
@@ -9,7 +9,6 @@ export default class TeqFw_Core_App_Defaults {
 
     BACK_REALM = 'core';  // realm for API services ('/api/core/...') and CLI commands ('core-...')
 
-    DI_APP = 'coreApp'; // DI container label for Vue application singleton.
     DI_BOOTSTRAP = 'bootstrap'; // DI container label for bootstrap configuration.
     DI_CONFIG = 'coreConfig'; // TODO: should we use named singleton or instance 'TeqFw_Di_Container$'?
     DI_CONTAINER = 'container'; // DI container label for container itself (see TeqFw_Di_Container.constructor).
