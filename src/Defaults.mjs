24bfadac63c7df52041a939df2dfb6330bae7100
Alex Gusev <alex@flancer64.com>
2021-06-26 09:53:11 +0300

    WF-451 Change DEF.DI_BOOTSTRAP to DEF.DI.BOOTSTRAP

--- src/Defaults.mjs
+++ src/Defaults.mjs
@@ -2,12 +2,8 @@
  * Plugin level constants (hardcoded configuration).
  */
 export default class TeqFw_Core_Defaults {
-    API_LOAD_CFG = '/load/config';
 
     BACK_REALM = 'core';  // realm for API services ('/api/core/...') and CLI commands ('core-...')
-
-    DI_I18N = 'coreI18n';
-    DI_STORE = 'coreStore'; // TODO: move it to http2/vue plugin (see TeqFw_Http2_Front_Gate_Load_Config.gate).
 
     I18N_BACK = 'back'; // i18n realm for backend resources in plugin initializer
     I18N_FRONT = 'front'; // i18n realm for frontend resources in plugin initializer
