be9f9558670e63a46de126de2d6d06a436239f3b
Alex Gusev <alex@flancer64.com>
2021-09-17 15:51:05 +0300

    WF-493 Add unit tests to cast util

--- RELEASE.md
+++ RELEASE.md
@@ -4,7 +4,7 @@
 
 * docs for plugin's teq-descriptor;
 * fix errors stealing in `TeqFw_Core_Back_App`;
-* use `@teqfw/di.replace` in `./teqfw/json` as an object and not as an array;
+* use `@teqfw/di.replace` in `./teqfw.json` as an object and not as an array;
 * fix double initialization for plugins;
 * add path-to-name function to plugin registry (`TeqFw_Core_Back_Scan_Plugin_Registry`);
 * add backend only utils (`TeqFw_Core_Back_Util`);
---
+++ test/Shared/Util/Cast.test.mjs
@@ -0,0 +1,114 @@
+import testEnv from '../../TestEnv.mjs';
+import assert from 'assert';
+import {describe, it} from 'mocha';
+
+/** @type {TeqFw_Di_Shared_Container} */
+const container = await testEnv();
+const util = await container.get('TeqFw_Core_Shared_Util_Cast');
+
+/**
+ * This is a sample test unit. Base code for future tests in case it will be required.
+ */
+describe('TeqFw_Core_Shared_Util_Cast', function () {
+
+    describe('castArray', function () {
+        const fn = util.castArray;
+        it('has right classname', async () => {
+            assert.strictEqual(fn.name, 'TeqFw_Core_Shared_Util_Cast.castArray');
+        });
+        it('returns empty array for wrong input', async () => {
+            const res = fn(null);
+            assert(Array.isArray(res) & res.length === 0, 'expected empty array');
+        });
+        it('returns copy of input array', async () => {
+            const data = [1, 2, 3];
+            const res = fn(data);
+            assert(res !== data, 'expected result should not be equal to input data ');
+            assert(res.length === data.length, 'lengths of input & output arrays should be equal ');
+        });
+    });
+
+    describe('castBoolean', function () {
+        const fn = util.castBoolean;
+        it('has right classname', async () => {
+            assert.strictEqual(fn.name, 'TeqFw_Core_Shared_Util_Cast.castBoolean');
+        });
+        it('returns true', async () => {
+            const r1 = fn('true');
+            const r2 = fn('TRUE');
+            const r3 = fn('yes');
+            const r4 = fn('YES');
+            const r5 = fn(1);
+            assert(
+                (r1 === true)
+                && (r2 === true)
+                && (r3 === true)
+                && (r4 === true)
+                && (r5 === true)
+            );
+        });
+        it('returns false', async () => {
+            const r1 = fn(0);
+            const r2 = fn(null);
+            const r3 = fn(undefined);
+            assert(
+                (r1 === false)
+                && (r2 === false)
+                && (r3 === false)
+            );
+        });
+    });
+
+    describe('castBooleanIfExists', function () {
+        const fn = util.castBooleanIfExists;
+        it('has right classname', async () => {
+            assert.strictEqual(fn.name, 'TeqFw_Core_Shared_Util_Cast.castBooleanIfExists');
+        });
+        it('returns true', async () => {
+            const r1 = fn('true');
+            const r2 = fn('TRUE');
+            const r3 = fn('yes');
+            const r4 = fn('YES');
+            const r5 = fn(1);
+            assert(
+                (r1 === true)
+                && (r2 === true)
+                && (r3 === true)
+                && (r4 === true)
+                && (r5 === true)
+            );
+        });
+        it('returns false', async () => {
+            const r1 = fn(0);
+            const r2 = fn(null);
+            assert(
+                (r1 === false)
+                && (r2 === false)
+            );
+        });
+        it('returns undefined', async () => {
+            const r1 = fn(undefined);
+            assert((r1 === undefined));
+        });
+    });
+
+    describe('castEnum', function () {
+        const fn = util.castEnum;
+        it('has right classname', async () => {
+            assert.strictEqual(fn.name, 'TeqFw_Core_Shared_Util_Cast.castEnum');
+        });
+        it('returns value', async () => {
+            const data = 'value';
+            const enu = {KEY: 'value', OTHER: 'other'};
+            const res = fn(data, enu);
+            assert(res === data);
+        });
+        it('returns undefined', async () => {
+            const data = 'missed';
+            const enu = {KEY: 'value', OTHER: 'other'};
+            const res = fn(data, enu);
+            assert(res === undefined);
+        });
+    });
+
+});
