71c11d07c1c4471782faa63f055f6706a753c55e
Alex Gusev <alex@flancer64.com>
2021-10-15 16:58:49 +0300

    MF-115 Integrate @teqfw/user

--- src/Shared/Api/IAction.mjs
+++ src/Shared/Api/IAction.mjs
@@ -1,5 +1,9 @@
 /**
  * Abstraction of some functionality with one input & one output argument.
+ * Both input and output arguments are an objects.
+ *
+ * 'function' notation is better than 'class' notation for an actions but there was some
+ * troubles with 'Find Usages' operation in IDEA for 'function' notation.
  *
  * @interface
  */
