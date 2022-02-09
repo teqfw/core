79406b620fadc1401a9a03aed118cbeb52b8baf0
Alex Gusev <alex@flancer64.com>
2022-02-09 08:23:31 +0200

    WF-561 Refactor front (user) registration

--- src/Back/Config.mjs
+++ src/Back/Config.mjs
@@ -14,16 +14,16 @@ export default class TeqFw_Core_Back_Config {
         // EXTRACT DEPS
         /** @type {TeqFw_Core_Back_Defaults} */
         const DEF = spec['TeqFw_Core_Back_Defaults$'];
-        /** @type {TeqFw_Core_Back_Api_Dto_Formless.Factory} */
-        const fConfig = spec['TeqFw_Core_Back_Api_Dto_Formless#Factory$'];
         /** @type {typeof TeqFw_Core_Back_Api_Dto_App_Boot} */
         const Boot = spec['TeqFw_Core_Back_Api_Dto_App_Boot#'];
+        /** @type {TeqFw_Core_Shared_Dto_Formless} */
+        const dtoFormless = spec['TeqFw_Core_Shared_Dto_Formless$'];
 
         // DEFINE WORKING VARS / PROPS
         /** @type {TeqFw_Core_Back_Api_Dto_App_Boot} */
         const boot = new Boot();
-        /** @type {TeqFw_Core_Back_Api_Dto_Formless} storage for local configuration */
-        let local = fConfig.create();
+        /** @type {TeqFw_Core_Shared_Dto_Formless.Dto} storage for local configuration */
+        let local = dtoFormless.createDto();
 
 
         // DEFINE INSTANCE METHODS
@@ -56,7 +56,8 @@ export default class TeqFw_Core_Back_Config {
             const full = join(root, cfg);
             if (existsSync(full) && statSync(full).isFile()) {
                 const data = readFileSync(full);
-                local = fConfig.create(data.toString());
+                const obj = JSON.parse(data.toString());
+                local = dtoFormless.createDto(obj);
             }
         };
 
