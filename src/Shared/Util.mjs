12a65f167fd21336ff429980522a56b0b8de069a
Alex Gusev <alex@flancer64.com>
2022-02-16 20:26:44 +0200

    WF-570 Move format functions to TeqFw_Core_Shared_Util_Format

--- src/Shared/Util.mjs
+++ src/Shared/Util.mjs
@@ -84,138 +84,6 @@ function deepMerge(target, source) {
 }
 
 /**
- * Convert local date to YYYY/MM/DD.
- * @param {Date|string|null} dateIn
- * @return {string}
- * @memberOf TeqFw_Core_Shared_Util
- * @deprecated use TeqFw_Core_Shared_Util_Format.date
- */
-function formatDate(dateIn = null) {
-    /** @type {Date} */
-    const date = (dateIn) ?
-        (dateIn instanceof Date) ? dateIn : new Date(dateIn) :
-        new Date();
-    const y = date.getFullYear();
-    const m = `${date.getMonth() + 1}`.padStart(2, '0');
-    const d = `${date.getDate()}`.padStart(2, '0');
-    return `${y}/${m}/${d}`;
-}
-
-/**
- * Convert local date to YYYY/MM/DD HH:MM:SS.
- * @param {Date|string|null} dateIn
- * @param {boolean} withSeconds
- * @return {string}
- * @memberOf TeqFw_Core_Shared_Util
- * @deprecated TeqFw_Core_Shared_Util_Format.dateTime
- */
-function formatDateTime(dateIn = null, withSeconds = true) {
-    /** @type {Date} */
-    const date = (dateIn) ?
-        (dateIn instanceof Date) ? dateIn : new Date(dateIn) :
-        new Date();
-    const y = date.getFullYear();
-    const m = `${date.getMonth() + 1}`.padStart(2, '0');
-    const d = `${date.getDate()}`.padStart(2, '0');
-    const h = `${date.getHours()}`.padStart(2, '0');
-    const i = `${date.getMinutes()}`.padStart(2, '0');
-    const s = `${date.getSeconds()}`.padStart(2, '0');
-    const time = (withSeconds) ? `${h}:${i}:${s}` : `${h}:${i}`;
-    return `${y}/${m}/${d} ${time}`;
-}
-
-/**
- * Convert local time to HH:MM:SS.
- * @param {Date|string|null} dateIn
- * @param {boolean} withSeconds
- * @return {string}
- * @memberOf TeqFw_Core_Shared_Util
- * @deprecated use TeqFw_Core_Shared_Util_Format.time
- */
-function formatTime(dateIn = null, withSeconds = true) {
-    /** @type {Date} */
-    const date = (dateIn) ?
-        (dateIn instanceof Date) ? dateIn : new Date(dateIn) :
-        new Date();
-    const h = `${date.getHours()}`.padStart(2, '0');
-    const i = `${date.getMinutes()}`.padStart(2, '0');
-    const s = `${date.getSeconds()}`.padStart(2, '0');
-    return (withSeconds) ? `${h}:${i}:${s}` : `${h}:${i}`;
-}
-
-/**
- * Convert UTC date to YYYY/MM/DD.
- * @param {Date|string|null} dateIn
- * @return {string}
- * @memberOf TeqFw_Core_Shared_Util
- * @deprecated don't use it at all
- */
-function formatUtcDate(dateIn = null) {
-    /** @type {Date} */
-    const date = (dateIn) ?
-        (dateIn instanceof Date) ? dateIn : new Date(dateIn) :
-        new Date();
-    const y = date.getUTCFullYear();
-    const m = `${date.getUTCMonth() + 1}`.padStart(2, '0');
-    const d = `${date.getUTCDate()}`.padStart(2, '0');
-    return `${y}/${m}/${d}`;
-}
-
-/**
- * Convert input to 'X.XX CUR'.
- *
- * @param {number} amount
- * @param {string|null} cur
- * @return {string}
- * @memberOf TeqFw_Core_Shared_Util
- * @deprecated don't use it at all
- */
-function formatAmount(amount, cur = null) {
-    const normAmnt = round(amount);
-    const normCur = (typeof cur === 'string') ? cur : '';
-    return `${normAmnt.toFixed(2)} ${normCur.toUpperCase()}`
-}
-
-/**
- * Convert UTC date to YYYY/MM/DD HH:MM:SS.
- * @param {Date|string|null} dateIn
- * @return {string}
- * @memberOf TeqFw_Core_Shared_Util
- * @deprecated don't use it at all
- */
-function formatUtcDateTime(dateIn = null) {
-    /** @type {Date} */
-    const date = (dateIn) ?
-        (dateIn instanceof Date) ? dateIn : new Date(dateIn) :
-        new Date();
-    const y = date.getUTCFullYear();
-    const m = `${date.getUTCMonth() + 1}`.padStart(2, '0');
-    const d = `${date.getUTCDate()}`.padStart(2, '0');
-    const h = `${date.getUTCHours()}`.padStart(2, '0');
-    const i = `${date.getUTCMinutes()}`.padStart(2, '0');
-    const s = `${date.getUTCSeconds()}`.padStart(2, '0');
-    return `${y}/${m}/${d} ${h}:${i}:${s}`;
-}
-
-/**
- * Convert UTC time to HH:MM:SS.
- * @param {Date|string|null} dateIn
- * @return {string}
- * @memberOf TeqFw_Core_Shared_Util
- * @deprecated don't use it at all
- */
-function formatUtcTime(dateIn = null) {
-    /** @type {Date} */
-    const date = (dateIn) ?
-        (dateIn instanceof Date) ? dateIn : new Date(dateIn) :
-        new Date();
-    const h = `${date.getUTCHours()}`.padStart(2, '0');
-    const i = `${date.getUTCMinutes()}`.padStart(2, '0');
-    const s = `${date.getUTCSeconds()}`.padStart(2, '0');
-    return `${h}:${i}:${s}`;
-}
-
-/**
  * Return 'true' if `val` is empty.
  * @param {*} val
  * @returns {boolean}
@@ -237,42 +105,6 @@ function isObject(val) {
         (!Array.isArray(val)) &&
         (val !== null)
     );
-}
-
-/**
- * Parse some data as boolean or use default value.
- * @param {*} data
- * @param def
- * @return {boolean}
- * @memberOf TeqFw_Core_Shared_Util
- * @deprecated use TeqFw_Core_Shared_Util_Cast.castBoolean
- */
-function parseBoolean(data, def = false) {
-    let result = def;
-    if (
-        (data === true) ||
-        ((typeof data === 'string') && (
-            (data.toLowerCase() === 'true') ||
-            (data.toLowerCase() === 'yes')
-        )) ||
-        ((typeof data === 'number') && (
-            (data !== 0)
-        ))
-    ) {
-        result = true;
-    } else if (
-        (data === false) ||
-        ((typeof data === 'string') && (
-            (data.toLowerCase() === 'false') ||
-            (data.toLowerCase() === 'no')
-        )) ||
-        ((typeof data === 'number') && (
-            (data === 0)
-        ))
-    ) {
-        result = false;
-    }
-    return result;
 }
 
 /**
@@ -303,23 +135,14 @@ function str2ab(str) {
     return buf;
 }
 
-
 // finalize code components for this es6-module
 Object.defineProperty(ab2b64, 'name', {value: `${NS}.${ab2b64.name}`});
 Object.defineProperty(ab2hex, 'name', {value: `${NS}.${ab2hex.name}`});
 Object.defineProperty(ab2str, 'name', {value: `${NS}.${ab2str.name}`});
 Object.defineProperty(b642ab, 'name', {value: `${NS}.${b642ab.name}`});
 Object.defineProperty(deepMerge, 'name', {value: `${NS}.${deepMerge.name}`});
-Object.defineProperty(formatAmount, 'name', {value: `${NS}.${formatAmount.name}`});
-Object.defineProperty(formatDate, 'name', {value: `${NS}.${formatDate.name}`});
-Object.defineProperty(formatDateTime, 'name', {value: `${NS}.${formatDateTime.name}`});
-Object.defineProperty(formatTime, 'name', {value: `${NS}.${formatTime.name}`});
-Object.defineProperty(formatUtcDate, 'name', {value: `${NS}.${formatUtcDate.name}`});
-Object.defineProperty(formatUtcDateTime, 'name', {value: `${NS}.${formatUtcDateTime.name}`});
-Object.defineProperty(formatUtcTime, 'name', {value: `${NS}.${formatUtcTime.name}`});
 Object.defineProperty(isEmpty, 'name', {value: `${NS}.${isEmpty.name}`});
 Object.defineProperty(isObject, 'name', {value: `${NS}.${isObject.name}`});
-Object.defineProperty(parseBoolean, 'name', {value: `${NS}.${parseBoolean.name}`});
 Object.defineProperty(round, 'name', {value: `${NS}.${round.name}`});
 Object.defineProperty(str2ab, 'name', {value: `${NS}.${str2ab.name}`});
 
@@ -329,16 +152,8 @@ export {
     ab2str,
     b642ab,
     deepMerge,
-    formatAmount,
-    formatDate,
-    formatDateTime,
-    formatTime,
-    formatUtcDate,
-    formatUtcDateTime,
-    formatUtcTime,
     isEmpty,
     isObject,
-    parseBoolean,
     round,
     str2ab,
 };
