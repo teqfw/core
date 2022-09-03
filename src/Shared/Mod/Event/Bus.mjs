7aa24e17c6f56ce192b97f4934924ccf27929f9e
Alex Gusev <alex@flancer64.com>
2022-09-03 13:40:42 +0300

    Share program from edit/play modes
    link to issue: flancer32/pwa-ra-mob#105

--- src/Shared/Mod/Event/Bus.mjs
+++ src/Shared/Mod/Event/Bus.mjs
@@ -10,6 +10,8 @@
 export default class TeqFw_Core_Shared_Mod_Event_Bus {
     constructor(spec) {
         // DEPS
+        /** @type {TeqFw_Core_Shared_Api_ILogger} */
+        const logger = spec['TeqFw_Core_Shared_Api_ILogger$$']; // instance
         /** @type {typeof TeqFw_Core_Shared_Mod_Event_Subscription} */
         const Subscription = spec['TeqFw_Core_Shared_Mod_Event_Subscription#'];
 
@@ -17,14 +19,21 @@ export default class TeqFw_Core_Shared_Mod_Event_Bus {
         /** @type {Object<string, function[]>} */
         const _listeners = {};
 
+        // MAIN
+        logger.setNamespace(this.constructor.name);
+
         // INSTANCE METHODS
 
-        this.publish = function (message) {
+        this.publish = async function (message) {
             const eventName = message?.meta?.name;
             if (Array.isArray(_listeners[eventName]))
-                for (const one of _listeners[eventName]) {
-                    one(message);
-                }
+                for (const one of _listeners[eventName])
+                    one(message)
+                        ?.catch((e) => {
+                            // catch error in event processor (listener)
+                            logger.error(e);
+                            debugger;
+                        });
         }
 
         this.subscribe = function (eventName, handler) {
