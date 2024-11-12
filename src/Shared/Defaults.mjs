1b9501b22d00152916952322cf8e58934bb63d1c
Alex Gusev <alex@flancer64.com>
2024-11-12 17:21:30 +0200

    SVTR-147 Make a link to verify new email

--- src/Shared/Defaults.mjs
+++ src/Shared/Defaults.mjs
@@ -3,6 +3,7 @@
  */
 export default class TeqFw_Core_Shared_Defaults {
     DI_WRAP_PROXY = 'proxy'; // wrap DI result as factory proxy
+    // TODO: take this name from 'package.json'
     NAME = '@teqfw/core'; // plugin's node in 'teqfw.json' & '.cfg/local.json'
     NAME_DI = '@teqfw/di'; // DI plugin does not have own hardcoded defaults.
 
