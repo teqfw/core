d3d9fe0b5ac934d87c8acc99492a09c1d9ad4032
Alex Gusev <alex@flancer64.com>
2024-11-28 19:43:52 +0200

    OWN-358 Set up env for GPT user demo

--- src/Shared/App/Di/PostProcessor/Proxy.mjs
+++ src/Shared/App/Di/PostProcessor/Proxy.mjs
@@ -38,10 +38,16 @@ export default class TeqFw_Core_Shared_App_Di_PostProcessor_Proxy {
             if (map[modOrig]) {
                 // there are proxies for the original object
                 for (const modReplace of map[modOrig]) {
-                    const wrapId = originalId.value.replace(modOrig, modReplace);
-                    /** @type {TeqFw_Core_Shared_Api_Di_Proxy} */
-                    const wrapper = await container.get(wrapId);
-                    res = wrapper.wrapOrigin(res);
+                    const parentIndex = stack.length - 2;
+                    if ((parentIndex >= 0) && (stack[parentIndex] === modOrig)) {
+                        // don't proxy the obj to prevent looping
+                    } else {
+                        const stackNew = [...stack, modOrig];
+                        const wrapId = originalId.value.replace(modOrig, modReplace);
+                        /** @type {TeqFw_Core_Shared_Api_Di_Proxy} */
+                        const wrapper = await container.get(wrapId, stackNew);
+                        res = (typeof wrapper.wrapOrigin === 'function') ? wrapper.wrapOrigin(res) : wrapper;
+                    }
                 }
             }
             return res;
